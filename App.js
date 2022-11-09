10701bc189956ce5fbd5a7b86db1df0d8c84c9d0
Maxim Stribylo <max057521@gmail.com>
2022-11-10 00:04:36 +0300

    update

--- App.js
+++ App.js
@@ -9,13 +9,13 @@ export default function App() {
       source={require("./assets/3060bf968d92368179ce26a756ce4271.jpg")}
     >
       <View style={styles.container}>
-        <View style={styles.innerBox}>
-          <Text style={styles.text}>
-            Open up App.js to start working on your app!
-          </Text>
-        </View>
-        <StatusBar style="auto" />
+        {/* <View style={styles.innerBox}> */}
+        {/* <Text style={styles.text}> */}
+        {/* Open up App.js to start working on your app! */}
+        {/* </Text> */}
       </View>
+      <StatusBar style="auto" />
+      {/* </View> */}
     </ImageBackground>
 
     // </View>
@@ -24,23 +24,23 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    // backgroundColor: "#fff",
+    // flex: 1,
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    color: "teal",
-    fontSize: 50,
-    textAlign: "center",
-  },
-  innerBox: {
-    borderWidth: 3,
-    borderColor: "red",
-    padding: 40,
-    borderRadius: 40,
-    width: 350,
-  },
+  // text: {
+  //   color: "teal",
+  //   fontSize: 50,
+  //   textAlign: "center",
+  // },
+  // innerBox: {
+  //   borderWidth: 3,
+  //   borderColor: "red",
+  //   padding: 40,
+  //   borderRadius: 40,
+  //   width: 350,
+  // },
   image: {
     flex: 1,
     resizeMode: "cover",
